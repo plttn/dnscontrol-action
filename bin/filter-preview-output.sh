0e08c34e7461f7a182ea441c1bfdd42b9c4e2baf
Jack Platten <jack@platten.me>
2024-11-08 20:05:01 -0800

    remove for now

--- bin/filter-preview-output.sh
+++ bin/filter-preview-output.sh
@@ -6,9 +6,6 @@ grep -v -e '^\.\.\.0 corrections$' |\
   grep -v -e '\.\.\. (skipping)' |\
   grep -v -e '^----- DNS Provider: ' |\
   grep -v -e '^----- Registrar: ' |\
-  grep -v -e -i '^Concurrently ' |\
-  grep -v -e -i '^Serially ' |\
-  grep -v -e '^Waiting for concurrent ' |\
   grep -v -e '^----- Getting nameservers from:' | \
   sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g" # remove ANSI color codes
   ## https://stackoverflow.com/questions/17998978/removing-colors-from-output
